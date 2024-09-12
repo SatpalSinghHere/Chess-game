0e5728a059664a96e9a08344df2ffb0b56723f4e
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-09-12 14:24:12 +0530

    added render domain in settings.py

--- chess/settings.py
+++ chess/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-zr=^b)@wdwdp^(jm@j47ot*2@zo)k7tzd(&4&r2qx^2(&ali97
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.1.112']
+ALLOWED_HOSTS = [
+    'chess-game-x4o4.onrender.com',  # Add your Render domain here
+    'localhost',
+    '127.0.0.1',
+    # Add any other domains or IPs you need to allow
+]
 
 
 # Application definition
